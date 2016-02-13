3fa43cb49cdb22ed1a58a121db1c3cabc4c24dce
PachydermX <renjimin@gmail.com>
2016-02-13 22:13:42 +0900

    Player Switch

---
+++ Assets/Ball.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	private SpriteRenderer default_ball;
+	private SpriteRenderer p1_ball;
+	private SpriteRenderer p2_ball;
+
+	// Use this for initialization
+	void Start () {
+		default_ball = this.gameObject.GetComponent<SpriteRenderer>();
+		p1_ball = this.gameObject.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>();
+		p2_ball = this.gameObject.transform.GetChild(1).gameObject.GetComponent<SpriteRenderer>();
+
+		Player(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Player (int id) {
+		default_ball.enabled = false;
+		p1_ball.enabled = false;
+		p2_ball.enabled = false;
+
+		switch(id) {
+		case 1:
+			p1_ball.enabled = true;
+			break;
+		case 2:
+			p2_ball.enabled = true;
+			break;
+		default:
+			default_ball.enabled = true;
+			break;
+		}
+	}
+}
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -2,14 +2,40 @@
 using System.Collections;
 
 public class Main : MonoBehaviour {
+	public GameObject ball;
+	private Rigidbody2D ball_rb;
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log("Hello world");
+		ball_rb = ball.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		// Debug Mode
+		// the force
+		int force = 100;
+		if (Input.GetKeyDown(KeyCode.UpArrow)){
+			ball_rb.AddForce(new Vector2(0, force));
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow)){
+			ball_rb.AddForce(new Vector2(0, -force));
+		}
+		if (Input.GetKeyDown(KeyCode.LeftArrow)){
+			ball_rb.AddForce(new Vector2(-force, 0));
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow)){
+			ball_rb.AddForce(new Vector2(force, 0));
+		}
+
+		// ball sw
+		if (Input.GetKeyDown(KeyCode.Alpha1)){
+			ball.SendMessage("Player", 1);
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha2)){
+			ball.SendMessage("Player", 2);
+		}
 	}
+	
 }
