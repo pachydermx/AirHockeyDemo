33f03a39da57e056d905e6c9c8442c5a498a92f1
PachydermX <renjimin@gmail.com>
2016-02-14 22:25:37 +0900

    Moving Accelerator

--- Assets/Scripts/Accelerator.cs
+++ Assets/Scripts/Accelerator.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Accelerator : MonoBehaviour {
 	public GameObject manager;
+	private float move_speed = 2.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -11,7 +12,27 @@ public class Accelerator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		// control
+		if (Input.GetKey (KeyCode.W)){
+			transform.Translate(Vector3.up * move_speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.S)){
+			transform.Translate(Vector3.up * -move_speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.D)){
+			transform.Translate(Vector3.right * move_speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.A)){
+			transform.Translate(Vector3.right * -move_speed * Time.deltaTime);
+		}
 	
+		if (Input.GetKey (KeyCode.Q)){
+			transform.Rotate(Vector3.forward * move_speed * 30 * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.E)){
+			transform.Rotate(Vector3.forward * -move_speed * 30 * Time.deltaTime);
+		}
 	}
 
 	void OnTriggerStay2D(Collider2D collider) {
