2430a106d4b6fc85f34d6b8d91989390b4eba24c
TakuyaYamamoto <4659.hack.vol.1_-_yamatti@ezweb.ne.jp>
2016-03-24 15:40:33 +0900

    TCP通信

---
+++ Assets/TCPCommunicator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+
+public class TCPCommunicator : MonoBehaviour {
+    string host = "192.168.1.190";
+    int port = 10001;
+    static TcpClient tcp;
+    Thread thread;
+
+    bool msg = false;
+
+    int fuel = 50;
+
+    // Use this for initialization
+    void Start () {
+        tcp = new TcpClient(host, port);
+        tcp.Client.ReceiveTimeout = 1000;
+
+        tcp.Connect(host, port);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    void controlSpray(int dir) // yama 0321
+    {
+        String mes = dir.ToString();
+        byte[] umsg = Encoding.UTF8.GetBytes(mes);
+        //byte[] umsg = Encoding.UTF8.GetBytes("1");
+        //tcp.Send(umsg, umsg.Length);
+        //tcp.
+        NetworkStream stream = tcp.GetStream();
+        stream.Write(umsg, 0, umsg.Length);
+        Debug.Log("dir = " + mes);
+    }
+}
